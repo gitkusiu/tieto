d779c9479b66621e7b9e2213087803dd3e1adada
Krzysztof Kosmider <kkusiu@gmail.com>
2016-09-08 09:55:50 +0200

    main.cpp

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+
+int N, Q;
+
+int main()
+{
+    std::cin >> N;
+    std::cin >> Q;
+    
+    std::cout << N << Q;
+    
+    for (int i=0; i<N; i++)
+    {
+        std::cout << i;
+    }
+
+    for (int i=0; i<Q; i++)
+    {
+        std::cout << i;
+    }
+}
