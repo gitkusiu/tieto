c0851e730a3f36e1e7428303436f73f7a087268e
Krzysztof Kosmider <kkusiu@gmail.com>
2016-09-20 11:02:52 +0200

    sss

--- gtest/test1.cpp
+++ gtest/test1.cpp
@@ -110,35 +110,116 @@ TEST(TagParserTest, ClosingParseNextLine)
     vector<Tag> tags = parser.GetTags();
     EXPECT_EQ(1, tags.size());
 
-    Tag x = tags[0];
-    EXPECT_EQ(0, x.GetSubTags().size());
+    Tag t = tags[0];
+    EXPECT_EQ(0, t.GetSubTags().size());
 
-    vector<PairStr> l_atrs = x.GetAtributes();
+    vector<PairStr> l_atrs = t.GetAtributes();
     EXPECT_EQ(1,                l_atrs.size());
     EXPECT_EQ("value",          l_atrs[0].first);
     EXPECT_EQ("\"HelloWorld\"", l_atrs[0].second);
 
+//--------------------------------------
+    parser.ParseNextLine("<tag2 name = \"Name1\">");
 
+    tags = parser.GetTags();
+    EXPECT_EQ(1, tags.size());
 
-//    parser.ParseNextLine("<tag2 name = \"Name1\">");
+    t = tags[0];
+    l_atrs = t.GetAtributes();
+    EXPECT_EQ(1,                l_atrs.size());
+    EXPECT_EQ("value",          l_atrs[0].first);
+    EXPECT_EQ("\"HelloWorld\"", l_atrs[0].second);
 
-//    tags = parser.GetTags();
-//    EXPECT_EQ(1, tags.size());
+    vector<Tag> subs = t.GetSubTags();
+    l_atrs = subs[0].GetAtributes();
+    EXPECT_EQ(1,                subs.size());
+    EXPECT_EQ(false,            subs[0].HasBeenClosed());
+    EXPECT_EQ(1,                l_atrs.size());
+    EXPECT_EQ("name",           l_atrs[0].first);
+    EXPECT_EQ("\"Name1\"", l_atrs[0].second);
 
-//    x = tags[0];
-//    EXPECT_EQ(1, x.GetSubTags().size());
-//    
-//    l_atrs = x.GetAtributes();
-//    EXPECT_EQ(1,                l_atrs.size());
-//    EXPECT_EQ("value",          l_atrs[0].first);
-//    EXPECT_EQ("\"HelloWorld\"", l_atrs[0].second);
-//    
-//    vector<Tag> subtags = x.GetSubTags();
-    
+//--------------------------------------
+    parser.ParseNextLine("</tag2>");
+    EXPECT_EQ(false, parser.GetTags()[0].HasBeenClosed());
+    EXPECT_EQ(true,  parser.GetTags()[0].GetSubTags()[0].HasBeenClosed());
+    parser.ParseNextLine("</tag1>");
+    EXPECT_EQ(true,  parser.GetTags()[0].HasBeenClosed());
+    EXPECT_EQ(true,  parser.GetTags()[0].GetSubTags()[0].HasBeenClosed());
 }
 
 
 
+
+
+
+  // <TechnicalDetails>
+  //
+  // EXPECT_EQ(expected, actual) is the same as
+  //
+  //   EXPECT_TRUE((expected) == (actual))
+  //
+  // except that it will print both the expected value and the actual
+  // value when the assertion fails.  This is very helpful for
+  // debugging.  Therefore in this case EXPECT_EQ is preferred.
+  //
+  // On the other hand, EXPECT_TRUE accepts any Boolean expression,
+  // and is thus more general.
+  //
+  // </TechnicalDetails>
+
+
+//// Tests factorial of 0.
+//TEST(FactorialTest, Zero) {
+//  EXPECT_EQ(1, Factorial(0));
+//}
+
+//// Tests factorial of positive numbers.
+//TEST(FactorialTest, Positive) {
+//  EXPECT_EQ(1, Factorial(1));
+//  EXPECT_EQ(2, Factorial(2));
+//  EXPECT_EQ(6, Factorial(3));
+//  EXPECT_EQ(40320, Factorial(8));
+//}
+
+
+//// Tests IsPrime()
+
+//// Tests negative input.
+//TEST(IsPrimeTest, Negative) {
+//  // This test belongs to the IsPrimeTest test case.
+
+//  EXPECT_FALSE(IsPrime(-1));
+//  EXPECT_FALSE(IsPrime(-2));
+//  EXPECT_FALSE(IsPrime(INT_MIN));
+//}
+
+//// Tests some trivial cases.
+//TEST(IsPrimeTest, Trivial) {
+//  EXPECT_FALSE(IsPrime(0));
+//  EXPECT_FALSE(IsPrime(1));
+//  EXPECT_TRUE(IsPrime(2));
+//  EXPECT_TRUE(IsPrime(3));
+//}
+
+//// Tests positive input.
+//TEST(IsPrimeTest, Positive) {
+//  EXPECT_FALSE(IsPrime(4));
+//  EXPECT_TRUE(IsPrime(5));
+//  EXPECT_FALSE(IsPrime(6));
+//  EXPECT_TRUE(IsPrime(23));
+//}
+
+// Step 3. Call RUN_ALL_TESTS() in main().
+//
+// We do this by linking in src/gtest_main.cc file, which consists of
+// a main() function which calls RUN_ALL_TESTS() for us.
+//
+// This runs all the tests you've defined, prints the result, and
+// returns 0 if successful, or 1 otherwise.
+//
+// Did you notice that we didn't register the tests?  The
+// RUN_ALL_TESTS() macro magically knows about all the tests we
+// defined.  Isn't this convenient?
 GTEST_API_ int main(int argc, char **argv) {
 
 
